3760619cdffe6eb59bc319861825fa736293d29b
Manu7864z <zropfmanuel@gmail.com>
2023-02-20 16:06:31 +0100

    styles for form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,6 +4,10 @@ import styled from "styled-components";
 export default function ActivityForm({ onAddActivity }) {
   const Form = styled.form`
     background-color: #3d2682;
+    display: grid;
+    grid-template-columns: 1fr;
+    align-items: center;
+    gap: 3px;
     border-radius: 10px;
     padding: 10px;
     margin: 10px;
@@ -35,8 +39,10 @@ export default function ActivityForm({ onAddActivity }) {
       <label htmlFor="input">Activity: </label>
       <input id="input" type="text" />
       <br />
-      <label htmlFor="goodWeather">Good Weather Activity: </label>
-      <input id="goodWeather" type="checkbox" />
+      <div>
+        <label htmlFor="goodWeather">Good Weather Activity: </label>
+        <input id="goodWeather" type="checkbox" />
+      </div>
       <button type="submit">Add</button>
     </Form>
   );
